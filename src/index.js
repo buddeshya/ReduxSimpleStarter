8e538c63161f2391a02c76663bc177f6b7eecc43
Uddeshya Barnwal <uddeshya.barnwal@valuence.inc>
2022-02-06 21:03:48 +0530

    Search Content Connected

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import _ from   'lodash';
 import ReactDOM, { render } from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
@@ -17,18 +18,27 @@ class App extends Component{
             selectedVideo : null
         });
 
-        YTSearch({key:API_KEY, term:"surfboards"}, (videos) =>
+        this.videoSearch('surfboards')
+
+        
+    }
+
+    videoSearch(term){
+        YTSearch({key:API_KEY, term: term}, (videos) =>
         {
             this.setState({
                 videos: videos,
                 selectedVideo: videos[0]
             });
         });
+        
     }
     render() {
+        const videoSearch = _.debounce((term)=> { this.videoSearch(term), 300});
+
     return (
         <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange = {videoSearch}/>
         <VideoDetail video = {this.state.selectedVideo} />
         <VideoList 
             onVideoSelect = {selectedVideo =>this.setState({selectedVideo})}
